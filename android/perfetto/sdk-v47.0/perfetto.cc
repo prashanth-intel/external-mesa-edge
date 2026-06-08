3bb5eaa33b3c4a2cc7619b8013ffe0ba1fa1a64d
prashanth-intel <prashanth.suresh@intel.com>
2026-06-08 08:36:25 +0000

    [COVERITY] external-mesa: fix insecure temporary file umask (1 finding)
    mkstemp() inherits the process umask, which may allow group/world read on the
    created temp file (CID 6179570, SECURE_TEMP). Restrict umask to 0177 before
    calling mkstemp() and restore it immediately after to ensure 0600 permissions.
    Tracked-on: NIACP3-1359
    Signed-off-by: Suresh, Prashanth <prashanth.suresh@intel.com>

--- android/perfetto/sdk-v47.0/perfetto.cc
+++ android/perfetto/sdk-v47.0/perfetto.cc
@@ -5996,7 +5996,9 @@ TempFile TempFile::Create() {
   temp_file.fd_.reset(_open_osfhandle(reinterpret_cast<intptr_t>(h), 0));
 #else
   temp_file.path_ = GetSysTempDir() + "/perfetto-XXXXXXXX";
+  const mode_t old_umask = umask(0177);
   temp_file.fd_.reset(mkstemp(&temp_file.path_[0]));
+  umask(old_umask);
   PERFETTO_CHECK(unlink(temp_file.path_.c_str()) == 0);
 #endif
   if (PERFETTO_UNLIKELY(!temp_file.fd_)) {
